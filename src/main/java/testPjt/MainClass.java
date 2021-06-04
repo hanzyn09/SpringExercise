fcd127e5bd29731f89ace97dd9268042858f6529
hanjin <ysurkkr12@naver.com>
2021-06-04 22:57:54 +0900

    add information

--- src/main/java/testPjt/MainClass.java
+++ src/main/java/testPjt/MainClass.java
@@ -3,7 +3,7 @@ package testPjt;
 import org.springframework.context.support.GenericXmlApplicationContext;
 
 public class MainClass {
-
+//프로젝트는 로컬에서 생성해서 import 해도 되고, 이클립스에서 maven 프로젝트를 만들어도 된다.
 	public static void main(String[] args) {
 		/*
 		 * TransportationWalk transportationWalk = new TransportationWalk();
